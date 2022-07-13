1e77ccefe1cad58f1076fe528fd21e447612de81
Karl Southern <karl@theangryangel.co.uk>
2022-07-13 10:36:11 +0100

    Violating PR, that should continue

---
+++ sample_module/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "version": "15.0.1.0.0",
+    "name": "sample_module",
+    "summary": "sample module to violate tests",
+    "author": "Glo Networks",
+    "website": "https://github.com/GlodoUK/oca_addons-repo-template-pr-144",
+    "depends": [
+        "base",
+    ],
+    "data": [],
+    "license": "Other proprietary",
+    "development_status": "Alpha",
+}
